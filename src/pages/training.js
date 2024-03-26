64ab5a0eb4b639d25c5bbab14ce2d5c026500e29
thisisobate <obasiuche62@gmail.com>
2024-03-26 21:27:46 +0100

    chore: fix data fetching
    Signed-off-by: thisisobate <obasiuche62@gmail.com>

--- src/pages/training.js
+++ src/pages/training.js
@@ -1,51 +1,55 @@
-import * as React from "react";
-import { useStaticQuery, graphql } from "gatsby";
-import Layout from '../components/layout';
-import Seo from '../components/seo';
-import thumbnail from "../images/thumbnail-default.png";
-import { Container } from "../components/ui/grid";
-import { Grid } from "../components/ui/grid";
-import CardPost from "../components/ui/card-post";
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import thumbnail from "../images/thumbnail-default.png"
+import { Container } from "../components/ui/grid"
+import { Grid } from "../components/ui/grid"
+import CardPost from "../components/ui/card-post"
 
-export default function TrainingPage ({location}) {
-    const query = useStaticQuery(
-        graphql`
-          query {
-            allMdx(
-              filter: { fileAbsolutePath: { regex: "/(content/trainings)/" } }
-              limit: 6
-            ) {
-              edges {
-                node {
-                    slug
-                    excerpt(pruneLength: 160)
-                    frontmatter {
-                      title
-                      url
-                      date(formatString: "MMMM DD, YYYY")
-                      description
-                      thumbnail {
-                        childImageSharp {
-                          gatsbyImageData(
-                            width: 400
-                            placeholder: BLURRED
-                            formats: [AUTO, WEBP, AVIF]
-                          )
-                        }
-                      }
-                    }
+export default function TrainingPage({ location }) {
+  const query = useStaticQuery(
+    graphql`
+      query {
+        allMdx(
+          filter: { fileAbsolutePath: { regex: "/(content/trainings)/" } }
+          sort: { order: ASC, fields: frontmatter___title }
+          limit: 6
+        ) {
+          edges {
+            node {
+              frontmatter {
+                title
+                site
+                description
+                thumbnail {
+                  childImageSharp {
+                    gatsbyImageData(
+                      width: 400
+                      placeholder: BLURRED
+                      formats: [AUTO, WEBP, AVIF]
+                    )
+                  }
                 }
               }
             }
           }
-        `);
-
-    return (
-        <Layout location={location}>
-            <Seo title='training and certification' description='Become a professional by getting certified in GitOps' url={location.href}
-        image={thumbnail} />
-        <Container className="pt-28 lg:pt-48">
-        <h1 className="mb-8 lg:mb-12 text-center lg:text-left">Training & Certification</h1>
+        }
+      }
+    `
+  )
+  return (
+    <Layout location={location}>
+      <Seo
+        title="training and certification"
+        description="Become a professional by getting certified in GitOps"
+        url={location.href}
+        image={thumbnail}
+      />
+      <Container className="pt-28 lg:pt-48">
+        <h1 className="mb-8 lg:mb-12 text-center lg:text-left">
+          Training & Certification
+        </h1>
         <Grid md={2} lg={3}>
           {query.allMdx.edges.map((item, index) => {
             return (
@@ -53,8 +57,7 @@ export default function TrainingPage ({location}) {
                 key={index}
                 title={item.node.frontmatter.title}
                 date={item.node.frontmatter.date}
-                excerpt={item.node.excerpt}
-                to={item.node.frontmatter.url}
+                to={item.node.frontmatter.site}
                 img={
                   item.node.frontmatter.thumbnail?.childImageSharp
                     .gatsbyImageData
@@ -63,7 +66,7 @@ export default function TrainingPage ({location}) {
             )
           })}
         </Grid>
-        </Container>
-        </Layout>
-    )
+      </Container>
+    </Layout>
+  )
 }
