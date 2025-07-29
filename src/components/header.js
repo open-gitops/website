e8a96000b22df2078fde846bc6c10fdaec5e6ac0
Dan Garfield <dan@codefresh.io>
2025-07-29 14:10:20 -0600

    Update banner for colocated events and remove gitopscon for now (#153)
    * Update banner for colocated events and remove gitopscon for now
    
    Signed-off-by: Dan Garfield <dan.garfield@octopus.com>
    
    * swap emoji
    
    Signed-off-by: Dan Garfield <dan.garfield@octopus.com>
    
    ---------
    
    Signed-off-by: Dan Garfield <dan.garfield@octopus.com>

--- src/components/header.js
+++ src/components/header.js
@@ -23,10 +23,10 @@ const Header = ({ color, hasBanner }) => {
       {hasBanner && (
         <div className="pb-4">
           <Banner
-            emoji={"🖥️"}
-            description={"Join us at GitOpsCon Europe 2025 VIRTUAL (May 28, 2025)"}
-            shortDescription={"Register Here!"}
-            announcementLink={"https://community.cncf.io/events/details/cncf-virtual-project-events-hosted-by-cncf-presents-gitopscon-europe-2025-virtual/"}
+            emoji={"☁️"}
+            description={"Join us in Atlanta for ArgoCon and FluxCon colocated with KubeCon! (Nov 10, 2025)"}
+            shortDescription={"Colocated Events Registration"}
+            announcementLink={"https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/co-located-events/about-co-located-events/"}
           />
         </div>
       )}
